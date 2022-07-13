44aacab808b86b4bbf6395ee6c84e6f3f7d09e05
AmirHossein-Rezaei <amirez.dev@gmail.com>
2022-07-13 10:13:05 +0430

    main page design completed(test)

--- app/src/main/java/com/amirez/krypto/MainActivity.kt
+++ app/src/main/java/com/amirez/krypto/MainActivity.kt
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.amirez.krypto.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
